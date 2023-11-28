eba1421158831fb596552c215fe19d2263e1dd2b
rajnish98 <rajnishpandey63@gmail.com>
2023-11-28 23:10:16 +0530

    PF

---
+++ condition/PF.java
@@ -0,0 +1,9 @@
+public class PF {
+  public static void main(String args[]) {
+    
+    int marks = 67;
+
+    String reportCard = marks >= 33 ? "pass" : "fail";
+    System.out.println(reportCard);
+  }
+}
