3163047a16e544a237240b101be73ca1a5dfcbde
rajnish98 <rajnishpandey63@gmail.com>
2023-11-29 20:59:35 +0530

    positive negative solution

---
+++ condition/positiNegat.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class positiNegat {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int number = sc.nextInt();
+
+    if (number > 0) {
+    System.out.println("The given number is posiive");
+      
+    }else {
+      System.out.println("The given is negative");
+    }
+  }
+  
+}
