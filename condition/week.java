1d2ded96715d96d01f11c2b693174f9ae8f756f4
rajnish98 <rajnishpandey63@gmail.com>
2023-11-29 21:27:24 +0530

    week

---
+++ condition/week.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class week {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter the week number (1-7) :");
+
+    int week = sc.nextInt();
+
+    switch (week) {
+      case 1: System.out.println("Monday");
+        break;
+      case 2: System.out.println("Tuesday");
+        break;
+      case 3: System.out.println("Wednesday");
+        break;
+      case 4: System.out.println("Thursday");
+        break;
+      case 5: System.out.println("Friday");
+        break;
+      case 6: System.out.println("Saturday");
+        break;
+      case 7: System.out.println("Sunday");
+        break;
+    
+      default: System.out.println("this is invalid! please enter week number between (1-7)");
+    }
+  }
+}
