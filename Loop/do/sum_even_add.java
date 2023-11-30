99710808fea5ad632e18f90b7243bcffdf6d5d13
rajnish98 <rajnishpandey63@gmail.com>
2023-12-01 01:44:40 +0530

    add even odd

---
+++ Loop/do/sum_even_add.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class sum_even_add {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+
+    int number;
+    int choice;
+    int evenSum = 0;
+    int oddSum = 0;
+
+    do {
+      System.out.println("Enter the number");
+      number = sc.nextInt();
+
+      if (number % 2 == 0) {
+        evenSum += number;
+        
+      }else{
+        oddSum += number;
+      }
+      System.out.println("Do you want to continue? Press 1 for yes or 0 for no");
+
+      choice = sc.nextInt();
+
+    }while(choice == 1);
+    System.out.println("sum of even numbers: " +evenSum);
+    System.out.println("sum of even numbers: " +oddSum);
+    
+
+  }
+}
