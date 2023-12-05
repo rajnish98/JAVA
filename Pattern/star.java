64993b71139c1777b421ffadcaeaea19ce32b3f8
rajnish98 <rajnishpandey63@gmail.com>
2023-12-05 21:12:36 +0530

    pattern

---
+++ Pattern/star.java
@@ -0,0 +1,12 @@
+// nested loop statment
+public class star {
+  public static void main(String args[]) {
+    int n = 4;
+    for (int line = 1; line <= n; line++){
+      for(int star = 1; star<= line; star++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
