2186d8ccbd04a1762717d0a5b7137477b07646f9
rajnish98 <rajnishpandey63@gmail.com>
2023-12-06 18:37:54 +0530

    Fun syntax with parameters

--- Function&Methods/functionsynt.java
+++ Function&Methods/functionsynt.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class functionsynt {
   public static void printHelloWorld(){
     System.out.println("Hello World");
@@ -5,7 +7,17 @@ public class functionsynt {
     System.out.println("Hello World");
     System.out.println("Hello World");
   }
+
+  public static int calculateSum(int num1, int num2) {// num1, num2 is parameter
+    int sum = num1 + num2;
+    return sum;
+  }
   public static void main(String args[]) {
-    printHelloWorld();
+    // printHelloWorld();
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int sum = calculateSum(a, b); // a, b is argument
+    System.out.println("sum is : " + sum);
   }
 }
