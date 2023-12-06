47a4b24aefc63bcb43c15818d777b67c7ae062fb
rajnish98 <rajnishpandey63@gmail.com>
2023-12-06 17:59:22 +0530

    Fun syntax

---
+++ Function&Methods/functionsynt.java
@@ -0,0 +1,11 @@
+public class functionsynt {
+  public static void printHelloWorld(){
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+  }
+  public static void main(String args[]) {
+    printHelloWorld();
+  }
+}
