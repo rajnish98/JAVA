76319c9ca3dcbb3a564d7007f4d2d01dc57b2e41
rajnish98 <rajnishpandey63@gmail.com>
2023-12-07 20:35:08 +0530

    factorial

---
+++ Function&Methods/Factorial.java
@@ -0,0 +1,18 @@
+public class Factorial {
+
+  public static int factorial(int n){
+    int f = 1;
+
+    for(int i = 1; i <= n; i++){
+      f = f * i;
+    }
+    return f;
+  }
+  public static void main(String[] args) {
+    // int fact = factorial(4);
+    // System.out.println("factorial of n is :" + " " + fact);
+
+    System.out.println(factorial(4));
+    
+  }
+}
