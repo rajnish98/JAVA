f5727af286b90b955c8ca77aa284a6a4acbcf919
rajnish98 <rajnishpandey63@gmail.com>
2023-12-09 22:17:40 +0530

    optimized prime number

--- Function&Methods/prime.java
+++ Function&Methods/prime.java
@@ -1,21 +1,35 @@
 public class prime {
+  // public static boolean isPrime(int n){
+  //   // conrner case
+  //   // 2
+  //   if(n == 2){
+  //     return true;
+  //   }
+  //   // boolean isPrime = true;
+  //   for(int i = 2; i<=n-1; i++){
+  //     if (n % i == 0) {
+  //       // isPrime = false;
+  //       // break;
+  //       return false;
+  //     }
+  //   }
+  //   // return isPrime;
+  //   return true;
+  // }
+
   public static boolean isPrime(int n){
-    // conrner case
-    // 2
-    if(n == 2){
+    if (n == 2) {
       return true;
     }
-    // boolean isPrime = true;
-    for(int i = 2; i<=n-1; i++){
-      if (n % i == 0) {
-        // isPrime = false;
-        // break;
+    for(int i = 2; i<=Math.sqrt(n); i++){
+      if(n % i == 0){
         return false;
       }
     }
-    // return isPrime;
     return true;
   }
+
+
 
   public static void main(String args[]) {
     System.out.println(isPrime(5));
