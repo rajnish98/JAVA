3af7e1e02b72ee09dc7e0c919216f896da87228a
rajnish98 <rajnishpandey63@gmail.com>
2023-12-26 22:03:26 +0530

    zero_to_one_traingle

---
+++ Pattern-II/zero_oneTraingle.java
@@ -0,0 +1,19 @@
+public class zero_oneTraingle {
+  public static void zero_Two(int n){
+    for (int i=1; i<=n; i++){
+      for (int j=1; j<=i; j++) {
+        if ((i+j) % 2 == 0) { // even
+          System.out.print("1");
+          
+        }else{
+          System.out.print("0");
+        }
+        
+      }
+      System.out.println();
+    }
+  }
+  public static void main(String args[]) {
+    zero_Two(5);
+  }
+}
